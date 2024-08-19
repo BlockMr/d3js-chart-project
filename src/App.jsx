1777f86b205759e16a718e5569c74a842055e957
andrei.lobanovich <andrey.lobanovich@actionengine.com>
2024-08-19 10:16:01 +0200

    make points add correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import "./App.css";
 import Container from "react-bootstrap/Container";
-import LinePlot from "./components/linePlot";
 import data from "./input";
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import LinePlot from "./components/LinePlot.jsx";
 
 export default function App() {
   const [all_data, set_all_data] = useState();
+  useEffect(() => {
+    function addPointsToData() {
+      const date = new Date();
+      const point_btc = {
+        date: date,
+        value: (Math.random() * 100 + 20).toFixed(2),
+      };
+      const point_eth = {
+        date: date,
+        value: (Math.random() * 100 + 20).toFixed(2),
+      };
+      data.charts[0].points.push(point_btc);
+      data.charts[1].points.push(point_eth);
+      // console.log(data)
+      set_all_data({ ...data });
+    }
 
-  function addPointsToData() {
-    const date = new Date();
-    const point_btc = {
-      date: date,
-      value: (Math.random() * 100 + 20).toFixed(2),
-    };
-    const point_eth = {
-      date: date,
-      value: (Math.random() * 100 + 20).toFixed(2),
-    };
-    data.charts[0].points.push(point_btc);
-    data.charts[1].points.push(point_eth);
-    console.log(data);
-  }
-
-  function updateData() {
-    setInterval(() => {
-      addPointsToData;
+    const interval = setInterval(() => {
+      addPointsToData();
     }, 1000);
-  }
 
-  updateData();
+    return () => clearInterval(interval);
+  }, [data]);
 
-  return <Container className="App"></Container>;
+  return <Container className="App">
+    <LinePlot data={all_data}/>
+  </Container>;
 }
--- src/components/LinePlot.jsx
+++ src/components/LinePlot.jsx
@@ -1,89 +1,91 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, {useRef} from "react";
 import * as d3 from "d3";
 
-export default function LinePlot({ data }) {
-  const svgRef = useRef(null);
+export default function LinePlot(data) {
+    console.log(data)
+    const svgRef = useRef(null);
 
-  const chartName = data.chart_name;
-  const charts = data.charts;
+    const chartName = data?.chart_name;
+    const charts = data?.charts || [];
 
-  useEffect(() => {
-    const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+
+    const margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
     const svg = d3
-      .select(svgRef.current)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+        .select(svgRef.current)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
     svg
-      .append("text")
-      .attr("x", width / 2)
-      .attr("y", margin.top / 1, 5)
-      .attr("text-anchor", "middle")
-      .style("font", "bold 30px sans-serif")
-      .attr("stroke", "white")
-      .text(chartName);
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", margin.top / 1, 5)
+        .attr("text-anchor", "middle")
+        .style("font", "bold 30px sans-serif")
+        .attr("stroke", "white")
+        .text(chartName);
 
     const x = d3
-      .scaleTime()
-      .domain(
-        d3.extent(
-          charts.flatMap((chart) => chart.points),
-          (p) => p.date
+        .scaleTime()
+        .domain(
+            d3.extent(
+                charts.flatMap((chart) => chart.points),
+                (p) => p.date
+            )
         )
-      )
-      .range([0, width]);
+        .range([0, width]);
 
     svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x).ticks(4));
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x).ticks(4));
 
     const y = d3
-      .scaleLinear()
-      .domain([
-        0,
-        d3.max(
-          charts.flatMap((chart) => chart.points),
-          (p) => p.value
-        ),
-      ])
-      .range([height, 0]);
+        .scaleLinear()
+        .domain([
+            0,
+            d3.max(
+                charts.flatMap((chart) => chart.points),
+                (p) => p.value
+            ),
+        ])
+        .range([height, 0]);
 
     svg.append("g").call(d3.axisLeft(y));
 
     charts.forEach((element) => {
-      const name = element.name;
-      const color = element.color;
-      const points = element.points;
-      try {
-        const points = points.slice(-100);
-      } catch (error) {}
+        const name = element.name;
+        const color = element.color;
+        const points = element.points;
+        try {
+            const points = points.slice(-100);
+        } catch (error) {
+        }
 
-      svg
-        .append("path")
-        .datum(points)
-        .attr("fill", "none")
-        .attr("stroke", color)
-        .attr("stroke-width", 1.5)
-        .attr(
-          "d",
-          d3
-            .line()
-            .x(function (p) {
-              return x(p.date);
-            })
-            .y(function (p) {
-              return y(p.value);
-            })
-        );
+        svg
+            .append("path")
+            .datum(points)
+            .attr("fill", "none")
+            .attr("stroke", color)
+            .attr("stroke-width", 1.5)
+            .attr(
+                "d",
+                d3
+                    .line()
+                    .x(function (p) {
+                        return x(p.date);
+                    })
+                    .y(function (p) {
+                        return y(p.value);
+                    })
+            );
     });
-  }, [data]);
 
-  return <svg ref={svgRef} width="500" height="500"></svg>;
+
+    return <svg ref={svgRef} width="500" height="500"></svg>;
 }
